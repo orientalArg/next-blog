fc3f8f726db04dc8239c5b1522ed07c74a07c64e
Joaquín R <elnacho357@gmail.com>
2022-05-26 18:22:09 -0300

    Update [slug].js

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -64,7 +64,7 @@ export const getServerSideProps = async pageContext => {
             props: {
                 body: post.body,
                 title: post.title,
-                
+                image:post.mainImage,
 
             }
         }
