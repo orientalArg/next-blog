00f95a968be3509f69fa6da1ffc57a6b4d7b4f91
Joaquín R <elnacho357@gmail.com>
2022-05-28 00:12:59 -0300

    Add files via upload

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,23 @@
 import { useRouter } from 'next/router';
 import styles from '../styles/Header.module.css'
+import Head from 'next/head'
 
 export const Header = () => {
     const router = useRouter();
 
     return (
+        <>
+        <Head>
+            <title>Military News</title>
+            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        </Head>
         <header className={styles.main}>
             <a onClick={() => router.push("/")}>Home</a>
             <a onClick={() => router.push("/about")}>About</a>
             <a>Tags</a>
             <a>Contact</a>
         </header>
+        </>
     )
 }
 
